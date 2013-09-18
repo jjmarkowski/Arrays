68566f1f64270b5dd975c45b348e3804da95720f
jjmarkowski <jjmarkow@comcast.net>
2013-09-18 15:54:47 -0600

    Added snakeDraft and rectangularize methods

--- multi.java
+++ multi.java
@@ -5,6 +5,8 @@ public class multi {
 						{6,7,8,9,20},
 						{11,12,13,14,15} };
 
+		int[] b = {1,2,3,4,5,6};
+
 		//sumRow(n)
 		//sumCol(n)
 		//sumTotal
@@ -13,11 +15,13 @@ public class multi {
 		//determinant()
 		//	-must be square
 
-		sumRow(a[][]);
+		print(snakeDraft(10, 3));
+
+		print(rectangularize(b, 3, 2));
 
 	}
 
-	public void print(int[][] a) {
+	public static void print(int[][] a) {
 		for (int r=0; r<a.length; r++) {
 			for (int c=0; c<a[r].length; c++) {
 				System.out.print(a[r][c] + " ");				
@@ -26,15 +30,48 @@ public class multi {
 		}
 	}
 
-	public static int sumRow(int[][] a) {
-		int sum = 0;
-		for (int r=0; r<a.length; r++) {
-			for (int c=0; c<a[r].length; c++) {
-				sum += a[r][c];
-				System.out.println("Sum of row" + a[r] + " = " + sum);				
+	public static int[][] snakeDraft(int players, int rounds) {
+		int pick = 1;
+		int[][] draft = new int[rounds][players];
+		for (int i=0; i<rounds; i++) {
+			if (i % 2 == 0) {
+				for (int i2=0; i2<players; i2++) {
+				draft[i][i2] = pick;
+				pick += 1;
+				}
+			} else {
+				for (int i2=players-1; i2>=0; i2--) {
+				draft[i][i2] = pick;
+				pick += 1;
+				}
 			}
-			System.out.println();
 		}
+		return draft;
 	}
+
+	public static int[][] rectangularize(int[] b, int rows, int col) {
+		int bValue = 0;
+		int[][] rectangularized = new int[rows][col];
+		for (int i=0; i<rows; i++) {
+			for (int i2=0; i2<col; i2++) {
+				rectangularized[i][i2] = b[bValue];
+				bValue += 1;
+			}		
+		}
+		return rectangularized;
+	}
+
+	// public static void sumRow(int[][] a) {
+	// 	int sum = 0;
+	// 	for (int r=0; r<a.length; r++) {
+	// 		for (int c=0; c<a[r].length; c++) {
+	// 			sum += a[r][c];
+	// 			System.out.println("Sum of row" + a[r] + " = " + sum);				
+	// 		}
+	// 		System.out.println();
+	// 	}
+	// 	return;
+	// }
+
 	
 }
